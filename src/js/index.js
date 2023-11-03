27f8715cca563a2e9e0fa0c5b0c9eb75da89f36d
Andleus <80576769+andleus@users.noreply.github.com>
2023-11-03 11:04:35 +0000

    Landing Page with React DONE

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,24 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import Navbar from "./component/Navbar.jsx";
+import Title from "./component/Title.jsx";
+import Cards from "./component/Cards.jsx";
+import Footer from "./component/Footer.jsx";
+
+
+function App(){
+    return(
+        <div>
+            <Navbar/>
+            <Title/>
+            <Cards/>
+            <Footer/>
+        </div>
+    )
+}
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<App />, document.querySelector("#app"));
+
