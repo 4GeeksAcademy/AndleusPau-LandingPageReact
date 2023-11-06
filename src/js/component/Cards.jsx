456de2b790a9b1cba105255c85cd52fee26afb5e
Pau Fargas <142971403+PauFargas@users.noreply.github.com>
2023-11-06 19:49:58 +0000

    [FEAT] Cards

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -3,29 +3,22 @@ import React from "react";
 
 const Cards = (props)=>{
     return(
-        <div className="container">
-            <div className="row justify-content-center bg-white p-5 mx-0">
-                <div className="row px-5 mt-0 border-bottom pb-5">
-                <div className="col-3 pb-5">
-                    <div className="card shadow-sm border-2">
-                        <img src={props.imageUrl} alt={props.alt}/>
-                        <div className="card-body">
-                            <h3 className="text-center">{props.title}</h3>
-                            <p className="text-center">{props.description}</p>
-                        </div>
-                        
-                        <div className="card-footer p-2 text-center">
-                            <a class="btn btn-primary" href={props.buttonLink} role="button">{props.buttonLabel}</a>
-                        </div>                        
-                    </div>
-                </div>
-                
-                </div>
-            </div>
+    <div className="container m-0">
+      <div className="row justify-content-center bg-white mx-0 px-0 pb-0">
+        <div className="card shadow-sm border-2 p-0">
+          <img src={props.imageUrl} alt={props.alt} />
+          <div className="card-body p-2">
+            <h3 className="text-center">{props.title}</h3>
+            <p className="text-center">{props.description}</p>
+          </div>
+          <div className="card-footer p-2 text-center">
+            <a className="btn btn-primary" href={props.buttonLink} role="button">
+              {props.buttonLabel}
+            </a>
+          </div>
         </div>
-    
-
-    );
-}
-
+      </div>
+    </div>
+  );
+};
 export default Cards;
--- src/js/index.js
+++ src/js/index.js
@@ -17,8 +17,51 @@ function App(){
     return(
         <div>
             <Navbar/>
-            <Title/>
-            <Cards/>
+            <Title/>            
+            <div className="container">
+                <div className="row mt-4 mb-5 p-0">
+                <div className="col-md-3">
+                    <Cards
+                    imageUrl="https://placeholder.shopfinejewelry.com/500x325"
+                    alt="Texto alternativo de la imagen"
+                    title="Card Title"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur sollicitudin justo. Nulla fermentum, lectus sed sodales eleifend, nisi velit ultrices turpis, et egestas neque est quis elit."
+                    buttonLink="#"
+                    buttonLabel="Tell Me More!"
+                    />
+                </div>
+                <div className="col-md-3">
+                    <Cards
+                    imageUrl="https://placeholder.shopfinejewelry.com/500x325"
+                    alt="Texto alternativo de la imagen"
+                    title="Card Title"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur sollicitudin justo. Nulla fermentum, lectus sed sodales eleifend, nisi velit ultrices turpis, et egestas neque est quis elit."
+                    buttonLink="#"
+                    buttonLabel="Tell Me More!"
+                    />
+                </div>
+                <div className="col-md-3">
+                    <Cards
+                    imageUrl="https://placeholder.shopfinejewelry.com/500x325"
+                    alt="Texto alternativo de la imagen"
+                    title="Card Title"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur sollicitudin justo. Nulla fermentum, lectus sed sodales eleifend, nisi velit ultrices turpis, et egestas neque est quis elit."
+                    buttonLink="#"
+                    buttonLabel="Tell Me More!"
+                    />
+                </div>
+                <div className="col-md-3">
+                    <Cards
+                    imageUrl="https://placeholder.shopfinejewelry.com/500x325"
+                    alt="Texto alternativo de la imagen"
+                    title="Card Title"
+                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur sollicitudin justo. Nulla fermentum, lectus sed sodales eleifend, nisi velit ultrices turpis, et egestas neque est quis elit."
+                    buttonLink="#"
+                    buttonLabel="Tell Me More!"
+                    />
+                </div>
+                </div>
+      </div>
             <Footer/>
         </div>
     )
